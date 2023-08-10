27da9b58083a88e924cdeae2013e76fb3a520f29
Nicolas Plaza <niplinig@espol.edu.ec>
2023-08-10 14:32:08 -0500

    Update index.pug

--- _config.ts
+++ _config.ts
@@ -1,5 +1,13 @@
 import lume from "lume/mod.ts";
+import pug from "lume/plugins/pug.ts";
 
 const site = lume();
+
+// optimize svgs
+// site.use(svgo());
+
+site.use(pug());
+
+site.copy("favicon.ico");
 
 export default site;
